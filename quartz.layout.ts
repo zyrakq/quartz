917082374b63211b6c8a686a4bd8c6eecda823b7
Erritis <serg.shehov@tutanota.com>
2024-10-23 02:40:53 +0300

    Add personal configurations and styles
    Added personal configurations and styles
    
    For a unique website style

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,8 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   afterBody: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/erritis/quartz"
     },
   }),
 }
@@ -17,7 +16,9 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.Breadcrumbs({
+      rootName: "Главная"
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
@@ -38,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  beforeBody: [Component.Breadcrumbs({ rootName: "Главная" }), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.Logo(),
     Component.MobileOnly(Component.Spacer()),
